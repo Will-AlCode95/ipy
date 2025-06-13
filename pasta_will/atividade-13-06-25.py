256eb76a5b140d9d7396de2381debd9dceddf1e2
Bill <williamvinicius007@gmail.com>
2025-06-13 11:55:09 -0300

    sdasd

---
+++ pasta_will/atividade-13-06-25.py
@@ -0,0 +1,26 @@
+import random
+
+class bau() :
+    def __init__(self):
+        self.bau = self
+   
+    itens = [
+    "Espada de Ferro",
+    "Escudo de Madeira",
+    "Poção de Cura",
+    "Arco Longo",
+    "Flecha de Aço",
+    "Elmo de Couro",
+    "Botas de Velocidade",
+    "Anel de Proteção",
+    "Capa da Invisibilidade",
+    "Machado de Guerra"
+     ]
+
+i1 = bau()
+i2 = bau()
+i3 = bau()
+
+print(i1)
+print(i2)
+print(i3)
